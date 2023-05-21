f6b19e3ee6a9908b0e4acfa7f7556b43b049b29d
Polymath Universata <polymathuniversata@outlook.com>
2023-05-21 18:11:35 +0000

    Works Fine

--- shell.c
+++ shell.c
@@ -1,25 +1,73 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - a simple shell that reads lines from the standard input
+ * free_data - frees data structure
  *
- * Return: Always 0.
+ * @datash: data structure
+ * Return: no return
  */
-int main(void)
+void free_data(data_shell *datash)
 {
-	char *line = NULL;
-	size_t len = 0;
-	ssize_t read;
+	unsigned int i;
 
-	while (1)
+	for (i = 0; datash->_environ[i]; i++)
 	{
-		printf("$ ");
-		read = getline(&line, &len, stdin);
-		if (read == -1)
-			break;
-		printf("%s", line);
+		free(datash->_environ[i]);
 	}
 
-	free(line);
-	return (0);
+	free(datash->_environ);
+	free(datash->pid);
+}
+
+/**
+ * set_data - Initialize data structure
+ *
+ * @datash: data structure
+ * @av: argument vector
+ * Return: no return
+ */
+void set_data(data_shell *datash, char **av)
+{
+	unsigned int i;
+
+	datash->av = av;
+	datash->input = NULL;
+	datash->args = NULL;
+	datash->status = 0;
+	datash->counter = 1;
+
+	for (i = 0; environ[i]; i++)
+		;
+
+	datash->_environ = malloc(sizeof(char *) * (i + 1));
+
+	for (i = 0; environ[i]; i++)
+	{
+		datash->_environ[i] = _strdup(environ[i]);
+	}
+
+	datash->_environ[i] = NULL;
+	datash->pid = aux_itoa(getpid());
+}
+
+/**
+ * main - Entry point
+ *
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: 0 on success.
+ */
+int main(int ac, char **av)
+{
+	data_shell datash;
+	(void) ac;
+
+	signal(SIGINT, get_sigint);
+	set_data(&datash, av);
+	shell_loop(&datash);
+	free_data(&datash);
+	if (datash.status < 0)
+		return (255);
+	return (datash.status);
 }
